9ccfa367e32301b6a6a7f9f88d1ed6e19580fb8b
SSY1203 <tlstjsdud566@gmail.com>
2022-12-29 13:15:17 +0900

    [수정] if문과 삼항연산자 학습

--- client/chapter/core/08-1.condition.js
+++ client/chapter/core/08-1.condition.js
@@ -2,10 +2,10 @@
 /* Condition                                                              */
 /* ---------------------------------------------------------------------- */
 
-let answer = +prompt('숫자를 입력해주세요', 0);
-let result = answer > 0 ? 1 : answer < 0 ? -1 : 0;
+// let answer = +prompt('숫자를 입력해주세요', 0);
+// let result = answer > 0 ? 1 : answer < 0 ? -1 : 0;
 // if (answer > 0) {
-console.log(result);
+// console.log(result);
 // } else if (answer < 0) {
 //   conso3le.log(result);
 // } else {
@@ -20,17 +20,27 @@ console.log(result);
 //       Yes | No
 
 // 영화 봤니?
-let didWatchMovie;
+let didWatchMovie = 'Yes';
 
 // 영화 볼거니?
-let goingToWatchMovie;
+let goingToWatchMovie = 'No';
 
 // if 문(statement)
-
-// else 절(caluse)
-
-// else if 복수 조건 처리
+if (didWatchMovie.includes('Yes')) {
+  console.log('그 영화 너무 재밌더라!');
+  // else if 복수 조건 처리
+} else if (goingToWatchMovie === 'Yes') {
+  console.log('같이 볼래?');
+  // else 절(caluse)
+} else {
+  console.log('재미 없을 거 같아ㅠ');
+}
 
 // 조건부 연산자
 
 // 멀티 조건부 연산자 식
+didWatchMovie.includes('Yes')
+  ? console.log('그 영화 너무 재밌더라!')
+  : goingToWatchMovie === 'Yes'
+  ? console.log('같이 볼래?')
+  : console.log('재미 없을 거 같아ㅜ');
