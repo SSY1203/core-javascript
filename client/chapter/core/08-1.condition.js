9a9d73bc316201ef5f0d7ab96bb40bffbb85c90b
SSY1203 <tlstjsdud566@gmail.com>
2022-12-29 11:50:34 +0900

    [추가] if문과 삼항연산자 학습

---
+++ client/chapter/core/08-1.condition.js
@@ -0,0 +1,36 @@
+/* ---------------------------------------------------------------------- */
+/* Condition                                                              */
+/* ---------------------------------------------------------------------- */
+
+let answer = +prompt('숫자를 입력해주세요', 0);
+let result = answer > 0 ? 1 : answer < 0 ? -1 : 0;
+// if (answer > 0) {
+console.log(result);
+// } else if (answer < 0) {
+//   conso3le.log(result);
+// } else {
+//   console.log(result);
+// }
+
+// 그 영화 봤니?
+//     ↓
+// Yes | No
+//     | 영화 볼거니?
+//           ↓
+//       Yes | No
+
+// 영화 봤니?
+let didWatchMovie;
+
+// 영화 볼거니?
+let goingToWatchMovie;
+
+// if 문(statement)
+
+// else 절(caluse)
+
+// else if 복수 조건 처리
+
+// 조건부 연산자
+
+// 멀티 조건부 연산자 식
