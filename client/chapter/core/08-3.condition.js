be77fc993a5f02a62e868995bba41e72b5dc1398
SSY1203 <tlstjsdud566@gmail.com>
2022-12-29 15:51:57 +0900

    [추가] switch문 학습 완성

--- client/chapter/core/08-3.condition.js
+++ client/chapter/core/08-3.condition.js
@@ -9,7 +9,7 @@ const MORNING = '아침',
   LATE_NIGHT = '심야',
   DAWN = '새벽';
 
-let thisTime = prompt('시간을 입력해주세요.', '');
+// let thisTime = prompt('시간을 입력해주세요.', '');
 
 /* 다양한 상황에 맞게 처리 --------------------------------------------------- */
 
@@ -30,41 +30,69 @@ let thisTime = prompt('시간을 입력해주세요.', '');
 // '한밤 중이거나, 새벽이니 아마도 꿈나라에 있을 것이다.'
 
 /* switch문 -------------------------------------------------------------- */
-switch (thisTime) {
-  case MORNING:
-    console.log('뉴스 기사 글을 읽는다.');
-    break;
-  case LUNCH:
-    console.log('자주 가는 식당에 가서 식사를 한다.');
-    break;
-  case DINNER:
-    console.log('동네 한바퀴를 조깅한다.');
-    break;
-  case NIGHT:
-    console.log('친구에게 전화를 걸어 수다를 떤다.');
-    break;
-  case LATE_NIGHT:
-  case DAWN:
-    console.log('한밤 중이거나, 새벽이니 아마도 꿈나라에 있을 것이다.');
-    break;
-  default:
-    console.log('올바른 시간대를 입력해주세요.');
-}
+// switch (thisTime) {
+//   case MORNING:
+//     console.log('뉴스 기사 글을 읽는다.');
+//     break;
+//   case LUNCH:
+//     console.log('자주 가는 식당에 가서 식사를 한다.');
+//     break;
+//   case DINNER:
+//     console.log('동네 한바퀴를 조깅한다.');
+//     break;
+//   case NIGHT:
+//     console.log('친구에게 전화를 걸어 수다를 떤다.');
+//     break;
+//   case LATE_NIGHT:
+//   case DAWN:
+//     console.log('한밤 중이거나, 새벽이니 아마도 꿈나라에 있을 것이다.');
+//     break;
+//   default:
+//     console.log('올바른 시간대를 입력해주세요.');
+// }
 
 /* switch문 → if문 변환 --------------------------------------------------- */
 
-if (thisTime === MORNING) {
-  console.log('뉴스 기사 글을 읽는다.');
-} else if (thisTime === LUNCH) {
-  console.log('자주 가는 식당에 가서 식사를 한다.');
-} else if (thisTime === DINNER) {
-  console.log('동네 한바퀴를 조깅한다.');
-} else if (thisTime === NIGHT) {
-  console.log('친구에게 전화를 걸어 수다를 떤다.');
-} else if (thisTime === LATE_NIGHT || thisTime === DAWN) {
-  console.log('한밤 중이거나, 새벽이니 아마도 꿈나라에 있을 것이다.');
-} else {
-  console.log('올바른 시간대를 입력해주세요.');
-}
+// if (thisTime === MORNING) {
+//   console.log('뉴스 기사 글을 읽는다.');
+// } else if (thisTime === LUNCH) {
+//   console.log('자주 가는 식당에 가서 식사를 한다.');
+// } else if (thisTime === DINNER) {
+//   console.log('동네 한바퀴를 조깅한다.');
+// } else if (thisTime === NIGHT) {
+//   console.log('친구에게 전화를 걸어 수다를 떤다.');
+// } else if (thisTime === LATE_NIGHT || thisTime === DAWN) {
+//   console.log('한밤 중이거나, 새벽이니 아마도 꿈나라에 있을 것이다.');
+// } else {
+//   console.log('올바른 시간대를 입력해주세요.');
+// }
 /* switch vs. if -------------------------------------------------------- */
 /* 위 상황에서는 Switch가 더 가독성이 좋다. */
+
+// 0 ~ 6의 값을 받는다.
+
+function getRandom(n) {
+  return Math.round(Math.random() * n);
+}
+
+// 임의 숫자를 넣어서 무슨 요일 인지를 알려주는 함수를 만들어보자.
+function getDay(number) {
+  switch (number) {
+    case 0:
+      return '일';
+    case 1:
+      return '월';
+    case 2:
+      return '화';
+    case 3:
+      return '수';
+    case 4:
+      return '목';
+    case 5:
+      return '금';
+    case 6:
+      return '토';
+  }
+}
+
+console.log(getDay(getRandom(6)));
