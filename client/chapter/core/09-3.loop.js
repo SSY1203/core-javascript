e4860535267f6f940cf71cabcc28978c93686c4c
SSY1203 <tlstjsdud566@gmail.com>
2022-12-30 14:36:08 +0900

    [추가] for문 학습

--- client/chapter/core/09-3.loop.js
+++ client/chapter/core/09-3.loop.js
@@ -1,18 +1,29 @@
 /* ---------------------------------------------------------------------- */
 /* For Loop                                                               */
 /* ---------------------------------------------------------------------- */
+/* 
+for (let j = 0; j < 10; j++) {
+  console.log(j);
+} */
 
 // 2 ~ 10까지의 짝수 출력하기
 
+for (let j = 1; j < 11; j++) {
+  if (j % 2 !== 0) {
+    continue;
+  }
+  // console.log(j);
+}
+
 const frontEndDev = 'HTML CSS SVG JavaScript jQuery React Redux'.split(' ');
 
-let i = 0;
-let l = frontEndDev.length;
+// let i = 0;
+// let l = frontEndDev.length;
 
-while (i < l) {
-  console.log(frontEndDev[i]);
-  i += 1;
-}
+// while (i < l) {
+// console.log(frontEndDev[i]);
+//   i += 1;
+// }
 
 // while 문 → for 문 (순환)
 // - 실행 흐름
@@ -20,8 +31,22 @@ while (i < l) {
 //   - 조건이 맞을 경우, 이어서(continue) 순환
 //   - 조건: SVG, jQuery는 출력하지 마세요.
 
+for (let i = 0; i < frontEndDev.length; i++) {
+  let result = frontEndDev[i];
+  if (result.includes('SVG') || result.includes('jQuery')) continue;
+
+  // console.log(result);
+}
+
 //   - 조건이 맞을 경우, 순환 중단(break)
 //   - 조건: JavaScript 까지만 출력하세요.
+
+for (let i = 0; i < frontEndDev.length; i++) {
+  let result = frontEndDev[i];
+  if (result.includes('jQuery')) break;
+
+  console.log(result);
+}
 
 //   - 무한 루프 (브레이크)
 //   - for 문 (역순환)
