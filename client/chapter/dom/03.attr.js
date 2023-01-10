8ad3f31620865596ac9315802411a76df5331f76
SSY1203 <tlstjsdud566@gmail.com>
2023-01-10 13:48:42 +0900

    [추가] attr 함수 학습

--- client/chapter/dom/03.attr.js
+++ client/chapter/dom/03.attr.js
@@ -57,34 +57,11 @@ first.dataset.play = 'playing'; // set
 console.log(first.dataset.play); // get
 
 // 유틸 함수
-// get 함수 만들기
-
-function getAttr(node, prop) {
-  if (typeof node === 'string') {
-    node = getNode(node);
-  }
-  return node.getAttribute(prop);
-}
-
 getAttr('.first', 'class'); // first
 
 // const second = getNode('.second');
 // console.assert(getAttr('.first', 'class') === 'first');
 
-// set 함수 만들기
-const setAttr = (node, prop, value) => {
-  if (typeof node === 'string') node = getNode(node);
-  if (typeof prop !== 'string')
-    throw new TypeError('setAttr 함수의 두 번째 인자는 문자 타입 이어야 합니다.');
-  if (prop.includes('data')) {
-    let rest = prop.slice(5);
-    node.dataset[rest] = value;
-  }
-  if (!value) throw new SyntaxError('setAttr 함수의 세 번째 인자는 필수값입니다.');
-  node.setAttribute(prop, value);
-};
-
 setAttr('.first', 'data-value', '안녕클레오파트라');
 
-// common 함수 만들기
-function attr(get, set) {}
+attr('.second', 'play', 'coding');
