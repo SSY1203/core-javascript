92721a79f760f32e5e729b3801ca590f407667eb
SSY1203 <tlstjsdud566@gmail.com>
2023-01-13 16:12:31 +0900

    [과제 완료]

--- client/main.js
+++ client/main.js
@@ -1,3 +1,34 @@
-import { getNode } from './lib/index.js';
+import {
+  getInputValue,
+  getNode,
+  insertLast,
+  attr,
+  clearContents,
+  syntaxError,
+  showAlert,
+} from './lib/index.js';
+import { jujeobData } from './data/data.js';
+import { getRandom } from './lib/math/index.js';
+import { isNumericString } from './lib/utils/index.js';
 
 const submit = getNode('#submit');
+const resultArea = getNode('.result');
+
+// 제출 버튼 핸들러
+function clickSubmitHandler(e) {
+  e.preventDefault();
+
+  let name = getInputValue('#nameField');
+  let list = jujeobData(name);
+  let pick = list[getRandom(list.length - 1)];
+
+  if (!name) {
+    showAlert('.alert-error', '잘못된 정보입니다!', 2000);
+    return;
+  }
+  if (isNumericString(name)) syntaxError('제대로 된 이름을 써주세요.');
+  clearContents(resultArea);
+  insertLast(resultArea, pick);
+}
+
+submit.addEventListener('click', clickSubmitHandler);
