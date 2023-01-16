d84cb9d3dff57e2248943a9a22771004580fa364
SSY1203 <tlstjsdud566@gmail.com>
2023-01-16 11:19:55 +0900

    [추가] toggle click

---
+++ client/main.js
@@ -0,0 +1,20 @@
+import { diceAnimation, getNode } from './lib/index.js';
+
+const rollingDiceButton = getNode('.buttonGroup > button:nth-child(1)');
+
+const handlerRollingDice = (() => {
+  let stopAnimation;
+  let isRolling = false;
+  // diceAnimation();
+
+  return () => {
+    if (!isRolling) stopAnimation = setInterval(diceAnimation, 100);
+    else clearInterval(stopAnimation);
+
+    isRolling = !isRolling;
+  };
+})();
+
+rollingDiceButton.addEventListener('click', handlerRollingDice);
+
+// let eventOff = bindEvent(rollingDiceButton, 'click', handlerRollingDice);
