c6f8cab2b66d7b46a5b80d4d5e8d8215d7016c01
SSY1203 <tlstjsdud566@gmail.com>
2023-01-17 15:07:52 +0900

    [추가] get, post, put, delete 함수 생성

--- client/lib/utils/xhr.js
+++ client/lib/utils/xhr.js
@@ -6,25 +6,87 @@
 4:complete      // 완료
  */
 
-export const xhrData = (method, url) => {
+export const xhrData = ({
+  url = '',
+  method = 'GET',
+  body = 'null',
+  onSuccess = null,
+  onFail = null,
+  headers = { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
+}) => {
   const xhr = new XMLHttpRequest();
 
   // 비동기 통신 오픈
   xhr.open(method, url);
 
+  console.log(Object.entries(headers));
+
+  //   Object.entries(headers).forEach(([key, value]) => {
+  //     xhr.setRequestHeader(key, value);
+  //   });
+
   // 변경이 될 때마다 호출
   xhr.addEventListener('readystatechange', () => {
-    if (xhr.status >= 200 && xhr.status < 400) {
-      if (xhr.readyState === 4) {
+    const { status, readyState, response } = xhr; // 객체 구조 분해 할당
+    if (status >= 200 && status < 400) {
+      if (readyState === 4) {
+        onSuccess(JSON.parse(response));
         console.log('통신 성공');
       }
     } else {
-      console.error('통신 실패');
+      onFail('통신 실패');
     }
   });
 
   // 서버에 요청
-  xhr.send();
+  xhr.send(JSON.stringify(body));
 };
 
-xhrData('GET', 'https://jsonplaceholder.typicode.com/users');
+xhrData.get = (url, onSuccess, onFail) => {
+  xhrData({
+    url,
+    onSuccess,
+    onFail,
+  });
+};
+
+xhrData.post = (url, body, onSuccess, onFail) => {
+  xhrData({
+    method: 'POST',
+    body,
+    url,
+    onSuccess,
+    onFail,
+  });
+};
+
+xhrData.put = (url, body, onSuccess, onFail) => {
+  xhrData({
+    method: 'PUT',
+    body,
+    url,
+    onSuccess,
+    onFail,
+  });
+};
+
+xhrData.delete = (url, onSuccess, onFail) => {
+  xhrData({
+    method: 'DELETE',
+    url,
+    onSuccess,
+    onFail,
+  });
+};
+
+// xhrData.delete(
+//   'https://jsonplaceholder.typicode.com/users/ 1',
+//   res => {
+//     console.log(res);
+//   },
+//   err => {
+//     console.log(err);
+//   },
+// );
+
+// console.dir(xhrData);
